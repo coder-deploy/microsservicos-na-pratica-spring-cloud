9e71f844e8d96e793570a0048be793b8ede1955b
Thiago Hernandes Souza <thiago.hernandes.souza@everis.com>
2019-07-29 17:31:22 -0300

    Ajustes no Zuul service

--- 02-transacoes-kafka/10-api-transacoes-financeiras/src/main/java/com/coder/deploy/controller/TransacaoController.java
+++ 02-transacoes-kafka/10-api-transacoes-financeiras/src/main/java/com/coder/deploy/controller/TransacaoController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.coder.deploy.domain.Transacao;
 import com.coder.deploy.service.TransacaoStreamService;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 
 @RestController
 @RequestMapping(value = "/api/transacoes")
@@ -17,8 +20,21 @@ public class TransacaoController {
 	@Autowired
 	private TransacaoStreamService transacaoService;
 	
+	@Autowired
+	private EurekaClient eurekaClient;
+	
+	private void portaClient() {
+	  Application application = eurekaClient.getApplication("TRANSACOES-FINANCEIRAS-SERVICE");
+	  InstanceInfo instanceInfo = application.getInstances().get(0);
+	  int port = instanceInfo.getPort();
+	  System.err.print("PORTA: -------------------" +port);
+		//
+	}
+	
+	
 	@PostMapping()
 	public ResponseEntity<Transacao> enviarTransacao(@RequestBody Transacao transacao) {
+		portaClient();
 		transacaoService.sendNotification(transacao);
 		return ResponseEntity.ok(
 				new Transacao(transacao.getData(), transacao.getValor(), transacao.getOperacao()));
